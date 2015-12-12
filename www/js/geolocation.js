78566f20c3af416b6d094492f6fbf7499983b2ea
marcioggs <marcioggs@gmail.com>
2015-12-12 21:25:23 -0200

    Cabeçalho fixo.

--- www/js/geolocation.js
+++ www/js/geolocation.js
@@ -1,14 +1,6 @@
 var geolocation = {
     exec: function() {
         var onSuccess = function(position) {
-            /*alert('Latitude: '          + position.coords.latitude          + '\n' +
-                'Longitude: '         + position.coords.longitude         + '\n' +
-                'Altitude: '          + position.coords.altitude          + '\n' +
-                'Accuracy: '          + position.coords.accuracy          + '\n' +
-                'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-                'Heading: '           + position.coords.heading           + '\n' +
-                'Speed: '             + position.coords.speed             + '\n' +
-                'Timestamp: '         + position.timestamp                + '\n');*/
 
             $("#latitude").val(position.coords.latitude);
             $("#longitude").val(position.coords.longitude);
@@ -21,8 +13,8 @@ var geolocation = {
         };
 
         function onError(error) {
-            alert('code: '    + error.code    + '\n' +
-                'message: ' + error.message + '\n');
+            alert("code: "    + error.code    + '\n' +
+                "message: " + error.message + '\n');
         }
 
         navigator.geolocation.getCurrentPosition(onSuccess, onError);
