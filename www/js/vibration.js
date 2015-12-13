7f3e33f381c0109fad19866aa34fe9a38a34ad3c
marcioggs <marcioggs@gmail.com>
2015-12-13 20:07:59 -0200

    Refatoração e cancelamento no vibration.

--- www/js/vibration.js
+++ www/js/vibration.js
@@ -1,8 +1,21 @@
 document.addEventListener("deviceready", function() {
 
     $(document).on("click", "#vibrate", function() {
-        var time = $("#time").val() * 1000;
+        var time = parseInt($("#time").val());
         navigator.vibrate(time);
+
+        $("#cancelVibration").button("enable");
+        setTimeout(function(){
+            $("#cancelVibration").button("disable");
+        }, time);
+    });
+
+    $(document).on("click", "#cancelVibration", function() {
+        navigator.vibrate(0);
+        //or navigator.vibrate([])
+        //or navigator.vibrate([0])
+
+        $("#cancelVibration").button("disable");
     });
 
 }, false);
